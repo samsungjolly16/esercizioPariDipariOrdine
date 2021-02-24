bd10e32e7b3f4dcf289a0d1fadd183f14e342629
tommaso lauria <tommaso.lauria9@gmail.com>
2021-02-24 13:11:58 +0100

    Add project files.

---
+++ Gestione/operazioni.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Gestione
+{
+    public class operazioni
+    {
+        public static void Riempimento(int[] array)
+        {
+            Random r = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(1, 100);
+            }
+        }
+        public static void PariDispari(int[] array)
+        {
+            int countPari = 0, countDispari = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                    countPari++;
+                else
+                    countDispari++;
+            }
+            int[] pari = new int[countPari];
+            int[] dispari = new int[countDispari];
+            int p = 0, d = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    pari[p] = array[i];
+                    p++;
+                }
+                else
+                {
+                    dispari[d] = array[i];
+                    d++;
+                }    
+            }
+            Console.WriteLine("l'array pari è:");
+            Stampa(pari);
+            Console.WriteLine("l'array dispari è:");
+            Stampa(dispari);
+        }
+        public static void Stampa(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+            Console.WriteLine($"{array[array.Length - 1]}.");
+        }
+        public static void BubbleSort1(int[] array)
+        {
+            int x, y;
+            int temp;
+            for (x = 0; x < array.Length - x; x++)
+            {
+                for (y = 0; y < array.Length - 1; y++)
+                {
+                    if (array[y] > array[y + 1])
+                    {
+                        temp = array[y];
+                        array[y] = array[y + 1];
+                        array[y + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ esercizioPariDipariOrdine/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using Gestione;
+
+namespace esercizioPariDipariOrdine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci la dimensione dell'array");
+            int k = int.Parse(Console.ReadLine());
+            int[] array = new int[k];
+            operazioni.Riempimento(array);
+            Console.WriteLine("l'array è : ");
+            operazioni.Stampa(array);
+            operazioni.PariDispari(array);
+            operazioni.BubbleSort1(array);
+            Console.Write($"l'array ordinato è: ");
+            operazioni.Stampa(array);
+            
+
+        }
+    }
+}
